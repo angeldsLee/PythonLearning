34da34ff3b386c89c1687b09b21c16245a2dc6f7
angeldslee <angeldslee@gmail.com>
2016-10-11 18:20:58 +0800

    ex48 up

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -1,25 +1,35 @@
+import re
 
 verb = ('go', 'stop', 'kill', 'eat')
 noun = ('door', 'bear', 'princess', 'cabinet')
+stop = ('the', 'in', 'of', 'from', 'at', 'it')
 dire = ('north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back')
 
-stuff = raw_input('input string or sth with space:')
+# stuff = raw_input('input string or sth with space:')
 
 def scan(stuff):
     res = []
     words = stuff.split()
     for word in words:
+        tmp = []
         if word in verb:
-            tmp = ('verb', '%r' % word)
+            tmp = ('verb', '%s' % word)
         elif word in noun:
-            tmp = ('noun', '%r' % word)
+            tmp = ('noun', '%s' % word)
         elif word in dire:
-            tmp = ('direction', '%r' % word)
-        elif typeof(word) == int:
-            tmp = ('num', '%r' % word)
+            tmp = ('direction', '%s' % word)
+        elif word.isdigit():
+            word = int(word)
+            tmp = ('number', '%d' % word)
+        elif word in stop:
+            tmp = ('stop', '%s' % word)
         else:
-            tmp = ('error', '%r' % word)
+            tmp = ('error', '%s' % word)
 
         res.append(tmp)
 
     return res
+
+test1 = scan("go north 23  344569 the")
+for test in test1:
+    print test
